e06449df83264591e341a34e05bb3acb212d25f1
victor-zeusAgro <victor.acrani@zeusagrotech.com>
2023-12-06 10:24:58 -0300

    feat(core): change semantic release config file

--- release.config.js
+++ release.config.js
@@ -17,7 +17,51 @@ const config = {
         },
       ],
       '@semantic-release/github',
+      ["semantic-release-unsquash", {
+        "commitAnalyzerConfig": {
+          "preset": "angular",
+          "parserOpts": {
+            "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+          }
+        },
+        "notesGeneratorConfig": {
+          "preset": "angular",
+          "parserOpts": {
+            "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+          },
+          "writerOpts": {
+            "commitsSort": ["subject", "scope"]
+          }
+        }
+      }]
     ],
   };
   
   module.exports = config;
+
+
+  /*
+  const config = {
+    branches: ['main'], // only run in "main" branch
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+      '@semantic-release/github',
+    ],
+  };
+  
+  module.exports = config;
+  */
