1769044f075a1c5048ca06d6dec7d6b40d518108
victor-zeusAgro <victor.acrani@zeusagrotech.com>
2023-12-06 14:30:20 -0300

    feat(): config semantic

--- release.config.js
+++ release.config.js
@@ -1,37 +1,36 @@
 const { execSync } = require('child_process');
 
 const config = {
-  branches: ['main'], // Ramificações para as quais o Semantic Release deve ser acionado
-  
-  // Plugins a serem utilizados
+  branches: ['main'],
+
   plugins: [
-    '@semantic-release/commit-analyzer', // Analisa os commits para determinar o tipo de versão
-    '@semantic-release/release-notes-generator', // Gera as notas de lançamento
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
-      '@semantic-release/changelog', // Atualiza o arquivo CHANGELOG.md
+      '@semantic-release/changelog',
       {
         changelogFile: 'CHANGELOG.md',
       },
     ],
     [
-      '@semantic-release/git', // Prepara o commit no repositório
+      '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
-    '@semantic-release/github', // Publica o lançamento no GitHub
+    '@semantic-release/github',
   ],
-  
+
   // Executa um comando de squash merge antes do Semantic Release
   prepare: [
     {
-      path: '@semantic-release/exec', // Utiliza o plugin exec
+      path: '@semantic-release/exec',
       cmd: 'git merge --squash origin/main',
     },
   ],
 
-  // Você também pode precisar ajustar o remote
+  // Ajusta o remote
   verifyRelease: {
     path: '@semantic-release/exec',
     cmd: 'git remote set-url origin https://github.com/victor-zeusAgro/test-versions.git',
@@ -39,6 +38,7 @@ const config = {
 };
 
 module.exports = config;
+
 
 
 
