2b89e8f910141c159176615b0fe59251dc551cea
victor-zeusAgro <victor.acrani@zeusagrotech.com>
2023-12-06 13:48:43 -0300

    feat(core): semantic release config update

--- release.config.js
+++ release.config.js
@@ -1,3 +1,34 @@
+const { execSync } = require('child_process');
+
+const config = {
+  branches: ['main'], // Apenas executa na branch "main"
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    // Script para realizar o squash dos commits antes do commit de lançamento
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        // Executa um script para realizar o squash dos commits antes do commit de lançamento
+        // Isso vai aglutinar os commits em um único commit de lançamento
+        // Certifique-se de ajustar o comando conforme necessário para o seu fluxo de trabalho
+        beforeRelease: 'git merge --squash HEAD~${commits.length} && git commit -m "chore(release): ${nextRelease.version}"',
+      },
+    ],
+  ],
+};
+
+module.exports = config;
+
+  /*
 const config = {
     branches: ['main'], // only run in "main" branch
     plugins: [
@@ -21,3 +52,4 @@ const config = {
   };
   
   module.exports = config;
+  */
