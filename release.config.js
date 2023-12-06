4ef0d705778d365fd4b869d40aa06d8493cd7e37
victor-zeusAgro <victor.acrani@zeusagrotech.com>
2023-12-06 09:41:49 -0300

    ci(core): update semantic release config file

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-    branches: ['main'],
+    branches: ['main'], // only run in "main" branch
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
@@ -21,21 +21,3 @@ const config = {
   };
   
   module.exports = config;
-  
-
-// const config = {
-//     branches: ['main'],
-//     plugins: [
-//         '@semantic-release/commit-analyzer',
-//         '@semantic-release/release-notes-generator',
-//         [
-//             "@semantic-release/git",{
-//                 "assets": ["dist/*.js", "dist/*.js.map"],
-//                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-//             }
-//         ],
-//         '@semantic-release/github'
-//     ]
-// };
-
-// module.exports = config;
