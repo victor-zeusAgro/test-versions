3a251fede62db2c1ba4a3a86eca3ce0ad0c63859
Victor Acrani <113617169+victor-zeusAgro@users.noreply.github.com>
2023-12-06 09:31:18 -0300

    Test branch (#1)
    * feat(core): add new greeting
    
    * docs(core): readme update

--- main.go
+++ main.go
@@ -9,5 +9,5 @@ func main(){
 	prof := flag.String("profession", "operator","professional to be greeted")
 	flag.Parse()
 	fmt.Println("smooooooooth ", prof)
-	
+	fmt.Println("hello smooooooooth ", prof)
 }
