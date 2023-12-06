ef08dc733221f84f33759a2bf0d4b7903babd5cf
victor-zeusAgro <victor.acrani@zeusagrotech.com>
2023-12-06 07:44:51 -0300

    feat(): semntic release updatre

--- release.config.js
+++ release.config.js
@@ -1,16 +1,41 @@
 const config = {
     branches: ['main'],
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        [
-            "@semantic-release/git",{
-                "assets": ["dist/*.js", "dist/*.js.map"],
-                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-            }
-        ],
-        '@semantic-release/github'
-    ]
-};
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+      '@semantic-release/github',
+    ],
+  };
+  
+  module.exports = config;
+  
 
-module.exports = config;
+// const config = {
+//     branches: ['main'],
+//     plugins: [
+//         '@semantic-release/commit-analyzer',
+//         '@semantic-release/release-notes-generator',
+//         [
+//             "@semantic-release/git",{
+//                 "assets": ["dist/*.js", "dist/*.js.map"],
+//                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+//             }
+//         ],
+//         '@semantic-release/github'
+//     ]
+// };
+
+// module.exports = config;
