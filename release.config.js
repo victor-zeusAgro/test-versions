36a3c8aafef98535e49c369714b9e467df036ab1
Victor Acrani <113617169+victor-zeusAgro@users.noreply.github.com>
2023-12-06 14:11:20 -0300

    feat():config (#7)
    * feat(): config
    
    * docs(): readme
    
    * ci(): plugings

--- release.config.js
+++ release.config.js
@@ -1,33 +1,45 @@
+const { execSync } = require('child_process');
+
 const config = {
-  branches: ['main'],
+  branches: ['main'], // Ramificações para as quais o Semantic Release deve ser acionado
+  
+  // Plugins a serem utilizados
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    '@semantic-release/commit-analyzer', // Analisa os commits para determinar o tipo de versão
+    '@semantic-release/release-notes-generator', // Gera as notas de lançamento
     [
-      '@semantic-release/changelog',
+      '@semantic-release/changelog', // Atualiza o arquivo CHANGELOG.md
       {
         changelogFile: 'CHANGELOG.md',
       },
     ],
     [
-      '@semantic-release/git',
+      '@semantic-release/git', // Prepara o commit no repositório
       {
         assets: ['CHANGELOG.md', 'package.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-        // Adicione a opção para utilizar o squash merge
-        // Substitua `develop` pelo nome da sua branch
-        // Certifique-se de que o squash merge está habilitado nessa branch
-        // Você também pode precisar ajustar o remote
-        'mergeStrategy': 'squash',
-        'mergeOptions': '--squash --no-ff --no-commit',
-        'releaseBranch': 'main'
       },
     ],
-    '@semantic-release/github',
+    '@semantic-release/github', // Publica o lançamento no GitHub
   ],
+  
+  // Executa um comando de squash merge antes do Semantic Release
+  prepare: [
+    {
+      path: '@semantic-release/exec', // Utiliza o plugin exec
+      cmd: 'git merge --squash origin/main',
+    },
+  ],
+
+  // Você também pode precisar ajustar o remote
+  verifyRelease: {
+    path: '@semantic-release/exec',
+    cmd: 'git remote set-url origin https://github.com/victor-zeusAgro/test-versions.git',
+  },
 };
 
 module.exports = config;
+
 
 
   /*
